350444c33305248f0b023f9e02f6b8a6b783617e
TienZhao <tienzhao@qq.com>
2019-02-28 00:05:39 +0800

    Exercise of Week01
    Create a webform 'output.aspx', and show some text with JS alert box, by configuring 'output.aspx.cs'. Upload to a GitHub repo in project 'HelloWorld'

---
+++ HelloWorld/output.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWorld00
+{
+    public partial class output : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("<script>alert('欢迎学习数据 思维下的算法与程序设计')</script>");
+        }
+    }
+}
