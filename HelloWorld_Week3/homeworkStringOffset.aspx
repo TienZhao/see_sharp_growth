100e1bf3e0c8f81f87ea22927cd50bbde218d263
TienZhao <tienzhao@qq.com>
2019-03-12 12:45:35 +0800

    Exercise of Week 03
    1. To create a pyramid composed of an increasing amount of capital letters, in alphabetical order. The number of rows are read from input.
    2. To aplly caesar encryption to the letters in the inout string, and a input offset value. The upper and lower case letters are in seperate loops.

---
+++ HelloWorld_Week3/homeworkStringOffset.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="homeworkStringOffset.aspx.cs" Inherits="HelloWorld.homeworkStringOffset" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:TextBox ID="txtInput" runat="server"></asp:TextBox>
+        <p>
+            <asp:TextBox ID="txtOffset" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="btnStart" runat="server" OnClick="btnStart_Click" Text="Start" />
+        <p>
+            <asp:TextBox ID="txtOutput" runat="server"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ HelloWorld_Week3/output.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWorld00
+{
+    public partial class output : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("<script>alert('欢迎学习数据 思维下的算法与程序设计')</script>");
+
+            //int a = 'A';
+            //Response.Write(a);
+
+            //int b;
+            //Response.Write(b);
+            //int声明没有初始值
+
+            int a = 10;
+            //int b = 3;
+            //double c = 3.0;
+
+            Response.Write(a+a+a+a);
+            //输出3
+
+            
+            Response.Write(a*4);
+            //输出3
+
+            Response.Write(a<<2);
+            //输出3.33
+        }
+    }
+}
